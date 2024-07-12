07f94dce551d8eb928fb97192c5d22af78d4d0d0
Hugo Larcher <hugo.larcher@huggingface.co>
2024-07-12 10:56:16 +0200

    WIP

--- load_tests/download_artifact.js
+++ load_tests/download_artifact.js
@@ -31,13 +31,47 @@ module.exports = async ({
         return;
     }
 
-    const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
+    const lastRelease = await github.rest.repos.getLatestRelease({
+        owner,
+        repo
+    });
+
+    const lastReleaseTag = lastRelease.data.tag_name;
+    const tagRef = `tags/${lastReleaseTag}`;
+    const lastReleaseCommit = await github.rest.git.getRef({
+        owner,
+        repo,
+        ref: tagRef
+    });
+    const lastReleaseSha = lastReleaseCommit.data.object.sha;
+    const lastReleaseRun = await github.rest.actions.listWorkflowRuns({
+        owner,
+        repo,
+        workflow_id: workflow.id,
+        sha: lastReleaseSha,
+        status: "success",
+        per_page: 1
+    });
+    const lastReleaseArtifacts = await github.rest.actions.listWorkflowRunArtifacts({
+        owner,
+        repo,
+        run_id: lastReleaseRun.data.workflow_runs[0].id
+    });
+
+    const lastArtifacts = await github.rest.actions.listWorkflowRunArtifacts({
         owner,
         repo,
         run_id: runs.data.workflow_runs[0].id
     });
 
-    const artifact = artifacts.data.artifacts.find(artifact => artifact.name === process.env.ARTIFACT_NAME);
+    const lastArtifact = lastArtifacts.data.artifacts.find(artifact => artifact.name === process.env.ARTIFACT_NAME);
+    const lastReleaseArtifact = lastReleaseArtifacts.data.artifacts.find(artifact => artifact.name === process.env.ARTIFACT_NAME);
+
+    await downloadArtifact(github, owner, repo, core, lastReleaseArtifact, lastReleaseTag);
+    await downloadArtifact(github, owner, repo, core, lastArtifact, artifact.workflow_run.head_sha);
+}
+
+async function downloadArtifact(github, owner, repo, core, artifact, suffix) {
     if (artifact) {
         const response = await github.rest.actions.downloadArtifact({
             owner,
@@ -48,7 +82,7 @@ module.exports = async ({
         require('fs').writeFileSync(process.env.ARTIFACT_FILENAME, Buffer.from(response.data));
         // create directory to unzip
         require('fs').mkdirSync(`${process.env.UNZIP_DIR}/${artifact.workflow_run.head_sha}`, {recursive: true});
-        require('child_process').execSync(`unzip -o ${process.env.ARTIFACT_FILENAME} -d ${process.env.UNZIP_DIR}/${artifact.workflow_run.head_sha}`);
+        require('child_process').execSync(`unzip -o ${process.env.ARTIFACT_FILENAME} -d ${process.env.UNZIP_DIR}/${suffix}`);
 
         console.log("Artifact downloaded successfully");
     } else {
